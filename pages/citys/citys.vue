ce7588c501c1961d0da0e9a39bda78999721c2a4
qiaozhididi <1343013603@qq.com>
2024-09-24 13:53:23 +0800

    首页轮播图

---
+++ pages/citys/citys.vue
@@ -0,0 +1,21 @@
+<template>
+	<uni-nav-bar dark :fixed="true" shadow background-color="#007AFF" status-bar left-icon="left" left-text="返回"
+		title="选择城市" @clickLeft="back" />
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,16 +1,35 @@
 <template>
-	<view class="content">
-
+	<view class="index-container">
+		<NavBar />
+		<view class="index-banner-box">
+			<swiper class="swiper" indicator-active-color="#00f783" :indicator-dots="true" :autoplay="true"
+				:interval="3000" :duration="3000">
+				<swiper-item>
+					<image class="banner" src="../../static/banner1.png" />
+				</swiper-item>
+				<swiper-item>
+					<image class="banner" src="../../static/banner1.png" />
+				</swiper-item>
+				<swiper-item>
+					<image class="banner" src="../../static/banner1.png" />
+				</swiper-item>
+			</swiper>
+		</view>
 	</view>
 </template>
 
 <script>
+	import NavBar from "../../components/navbar.vue"
 	export default {
+
 		data() {
 
 		},
 		onLoad() {
 
+		},
+		components: {
+			NavBar
 		},
 		methods: {
 
@@ -18,6 +37,30 @@
 	}
 </script>
 
-<style>
+<style lang="scss">
+	.index-container {
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
 
+		.index-banner-box {
+			display: flex;
+			width: 100%;
+			padding: 10px;
+			justify-content: center;
+			align-items: center;
+			border-radius: 5px;
+			overflow: hidden;
+
+			.swiper {
+				width: 100%;
+				height: 260rpx;
+
+				.banner {
+					width: 700rpx;
+					height: 260rpx;
+				}
+			}
+		}
+	}
 </style>
