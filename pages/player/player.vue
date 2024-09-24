3defc2eaffd3f5f7c41987472b2abff4328391fa
qiaozhididi <1343013603@qq.com>
2024-09-24 21:22:44 +0800

    播放页面

---
+++ pages/player/player.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<web-view
+			src="https://www.bilibili.com/video/BV1XT421r7xy/?vd_source=08e92f0b3925c21dbdeabf937f2221a3"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad(options) {
+			console.log(options.id)
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
